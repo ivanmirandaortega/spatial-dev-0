ce1aa3ed0b05d5dcaad677c158479013b2b0fb9f
ivanmirandaortega <imirandaortega.pd@gmail.com>
2023-07-31 21:10:13 -0700

    first commit

---
+++ hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Ivan Miranda Ortega  on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
